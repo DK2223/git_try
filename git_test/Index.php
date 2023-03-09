d2ffda789a205a2ccddc3debeb4a16747cad019f
DK2223 <127090099+DK2223@users.noreply.github.com>
2023-03-09 12:10:37 +0300

    Add files via upload

---
+++ git_test/Index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>HelloWorld</title>
+</head>
+<body>
+<p>HelloWorld_html</p>
+<p>HelloWorld_html</p>
+<?php echo'<p>HelloWorld_php</p>'; ?>
+</body>
+</html>
